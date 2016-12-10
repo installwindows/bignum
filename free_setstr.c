ff4e177fb4334202b673132e4ae2a724faf13076
Vincent Arnaud <varnaud@localhost.localdomain>
2016-12-10 09:21:54 -0800

    free

--- free_setstr.c
+++ free_setstr.c
@@ -6,8 +6,31 @@
 /*   By: varnaud <varnaud@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/10 07:23:55 by varnaud           #+#    #+#             */
-/*   Updated: 2016/12/10 07:58:54 by varnaud          ###   ########.fr       */
+/*   Updated: 2016/12/10 09:09:50 by varnaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "libft.h"
 
+char	*free_setstr(char *src, char *str, int order,
+	   	char *(*strfunc)(const char *, const char *))
+{
+	char	*tmp;
+
+	tmp = src;
+	src = order ? strfunc(src, str) : strfunc(str, src);
+	free(tmp);
+	return (src);
+}
+
+int		main(void)
+{
+	char	*src;
+
+	src = ft_itoa(1);
+	src = free_setstr(src, "-", 0, ft_strjoin);
+	printf("%s\n", src);
+}
